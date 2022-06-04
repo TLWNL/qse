c9b75e9aa71b6249eaff9cd5de3c6d2dcd41ec2a
TLWNL <dylanrijnsburger@gmail.com>
2022-06-03 17:40:17 -0700

    add all static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#mission'
-
+  
+  get '/mission', to: 'static_pages#mission'
   get '/team', to: 'static_pages#team'
   get '/project', to: 'static_pages#project'
   get '/glossary', to: 'static_pages#glossary'
-  
+  get '/resources', to: 'static_pages#resources'
+
 end
