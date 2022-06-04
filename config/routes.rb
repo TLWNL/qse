2a3de917aad800915f6c9c76feae8e00993c4d7c
TLWNL <dylanrijnsburger@gmail.com>
2022-06-03 21:51:30 -0700

    fix routes for zines

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :zines
   root 'zines#display'
   
-  get '/zine', to: 'zines#display'  
+  get '/zines', to: 'zines#display'  
   get '/mission', to: 'static_pages#mission'
   get '/team', to: 'static_pages#team'
   get '/project', to: 'static_pages#project'
   get '/glossary', to: 'static_pages#glossary'
   get '/resources', to: 'static_pages#resources'
   get '/joinus', to: 'static_pages#joinus'
+  
+  resources :zines
 end
